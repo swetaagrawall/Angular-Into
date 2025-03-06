3502bdb7088f5ea07ad5cc6ec7868b4b51106c0c
sweta <swetaarawal972@gmail.com>
2025-03-06 12:53:33 +0530

    [Sweta Agrawal]Add. uc2-BridgeLabz logo added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,16 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Hello from Bridgelabz';
+  title = 'angular-intro';
+  imgUrl = "BL-logo.jpg";
+
+  ngOnInit() {
+    this.title = "Hello from BridgeLabz";
+  }
 }
