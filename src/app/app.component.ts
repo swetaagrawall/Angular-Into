21fbf0935777041d2379984d00d049f04f346a63
sweta <swetaarawal972@gmail.com>
2025-03-06 13:10:35 +0530

    [Sweta Agrawal]Add. uc4-two way binding in string variable is added in the input box.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -12,6 +13,7 @@ export class AppComponent {
   title = 'angular-intro';
   imgUrl = "BL-logo.jpg";
   url = "https://www.bridgelabz.com";
+  userName: string = "";
 
   ngOnInit() {
     this.title = "Hello from BridgeLabz";
